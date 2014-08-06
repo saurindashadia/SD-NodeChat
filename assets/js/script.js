1975090eb4f3e4d4b1a1a86779ea160e7cf48abf
Saurin Dashadia(SD) <dev@saur.in>
2014-08-06 17:37:18 +0530

    Documented each variable, object and function. Separated some function for better API interface. window, document passed as plugin arguments. Some function name changed for better readability. Some socket event name changed for better readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,37 +6,193 @@
  * @license      http://opensource.org/licenses/MIT, see LICENSE
  */
 
-(function ($) {
+(function (window, document, $) {
+
     $.initSDNodeChat = function (options) {
-        // set default options.
-        //noinspection JSValidateTypes
-        var SDNodeChatConf = $.extend({
 
-            // Server ip/domain where chat resides, this should match the value from the config file.
-            server: "http://localhost",
+        var _defaults = {
 
-            // Port number to listen for communication, this should match value from the config file.
+            /**
+             * @default     localhost
+             * @type        Text
+             * @value       server-IP address
+             * @usage       This will be consider as default server for chat
+             */
+            server: "localhost",
+
+            /**
+             * @default     80
+             * @type        Text
+             * @value       Port number
+             * @usage       This will be consider as default port for the chat server
+             */
             port: "80",
 
-            // Chat container UL/OL
+            /**
+             * @default     $('#messages')
+             * @type        [object HTMLUListElement]
+             * @value       [object HTMLUListElement]
+             * @usage       This element will be consider as chat container
+             */
             chatContainer: $('#messages'),
 
-            // Id of the send button, this will be use bind sending action to this button.
+            /**
+             * @default     $('#SendButtonID')
+             * @type        [object HTMLButtonElement]
+             * @value       [object HTMLButtonElement]
+             * @usage       This element will be consider as an action to send chat
+             */
             sendButton: $('#SendButtonID'),
 
-            // Id of the textbox, this will be use to bind sending action to this text.
-            chatBox: $('#chatTextBoxId'),
+            /**
+             * @default     $('#chatTextBoxId')
+             * @type        [object HTMLInputElement]
+             * @value       [object HTMLInputElement]
+             * @usage       This element will be consider as chat input it can also handle action to send chat
+             */
+            chatBox: $('#chatTextBox'),
 
-            // Enable or disable enter key as sender on chatBox
+            /**
+             * @default     true
+             * @type        boolean
+             * @value       true/false
+             * @usage       This can enable or disable enter key as sender on chatBox
+             */
             enterToSend: true,
 
-            // Socket object
+            /**
+             * @default     $('#userContainer')
+             * @type        [object HTMLUListElement]
+             * @value       [object HTMLUListElement]
+             * @usage       This element will be consider as a container of online users list.
+             */
+            usersContainer: $('#userContainer'),
+
+
+            /**
+             * @default     null
+             * @type        [object Socket]
+             * @value       [object Socket]
+             * @usage       This socket object will be use to communicate to server.
+             */
             socket: null,
 
-            // Basic function to call on user action to send message.
-            // this will calll function to add message to chat container as well as
-            // will call function to push message to server
+            /**
+             * @default
+             * @returns {boolean}
+             */
+            message: {
+                sent: null,
+                received: null
+            },
+
+            /**
+             * @param       void
+             * @returns     void
+             * @usage       This function listen to "getIntro" event, which will basically trigger from server.
+             *              If server found connected client new/un-known, it will trigger "getIntro" event. Upon
+             *              "getIntro" event this function will call getIntro() function and finally it will fetch
+             *              friend-list
+             * @todo        Define this function private, this function should not be the part of API
+             */
+            _intro: function () {
+                var that = this;
+
+                that.socket.on('getIntro', function (data) {
+                    var clientName = that.getIntro();
+                    that.socket.emit('setIntro', {name: clientName});
+                })
+
+                // Request for friend-list
+                that.requestOnlineUsers();
+            },
+
+            /**
+             * @param       void
+             * @returns     string
+             * @usage       This function is to query clients basic info usually a name which can be use to identify
+             *              user in chat room
+             */
+            getIntro:function(){
+                return prompt('Please intro yourself to chatroom!', 'anonymous');
+            },
+
+            /**
+             * @param       void
+             * @returns     void
+             * @usage       This function is use to request list of online users
+             */
+            requestOnlineUsers: function(){
+                this.socket.emit('requestOnlineUsers');
+            },
+
+            /**
+             * @param       void
+             * @returns     void
+             * @usage       This function is use to set event listener for "receiveOnlineUsers"
+             */
+            receiveOnlineUsers: function () {
+                var that = this;
+                that.socket.on('receiveOnlineUsers', function (data) {
+                    that.displayUserList(data);
+                });
+            },
+
+            /**
+             * @param       data    {object}
+             * @returns     void
+             * @usage       This function is use to set friends list
+             */
+            displayUserList:function(data){
+                this.usersContainer.find('li').not('.caption').remove();
+                var userList = JSON.parse(data);
+                var userListHTML = '';
+                for(u in userList){
+                    userListHTML += '<li class="online">' + userList[u] + '</li>';
+                }
+                this.usersContainer.append($(userListHTML));
+            },
+
+            /**
+             * @param       void
+             * @returns     void
+             * @usage       This function is use to listen for new messages. Upon new message receive, call function to
+             *              add message to message container.
+             */
+            listenForMessages: function () {
+                var that = this;
+                that.socket.on('receiveMessage', function (data) {
+                    // sender should not receive/display the message.
+                    if (data.sender != this.io.engine.id) {
+                        that.addMessage(data.message, 'other');
+                    }
+                });
+            },
+
+            /**
+             *
+             * @param       message {text}
+             * @param       type    {text} ('self'/'other')
+             * @returns     void
+             * @usage       This function is use to display message.
+             *              There are two ways to display messages
+             *              1) type = 'self'    -> indicates message is sent by client itself.
+             *              2) type = 'other'   -> indicates message is received from server.
+             */
+            addMessage: function (message, type) {
+                var li = '<li class="' + type + '"><span>' + message + '</span></li>';
+                $(this.chatContainer).append($(li));
+            },
+
+
+            /**
+             * @param       void
+             * @returns     void
+             * @usage       This function will be called when user send message. This function then add message
+             *              to message container and will push message to server.
+             */
             sendMessage: function () {
+
                 var message = this.chatBox.val();
                 if (!message) return false;
 
@@ -47,76 +203,40 @@
                 this.chatBox.focus();
             },
 
-            // Add message to message container
-            addMessage: function (message, type) {
-                var li = '<li class="' + type + '"><span>' + message + '</span></li>';
-                $(this.chatContainer).append($(li));
-                $('#messages').scrollTop($('#messages').height());
-            },
-
-            // push message to server
+            /**
+             * @param       message {Text}
+             * @param       socket  {object}
+             * @returns     void
+             * @usage       This function will push a user message to server
+             */
             pushMessage: function (message, socket) {
                 message = message;
                 this.socket.emit('sendmessage', { message: message, sender: this.socket.io.engine.id });
-            },
-
-            listenForMessage: function () {
-                var that = this;
-                that.socket.on('getmessage', function (data) {
-                    if (data.sender != this.io.engine.id) {
-                        that.addMessage(data.message, 'other');
-                    }
-                });
-            },
-
-            intro: function () {
-                var that = this;
-                that.socket.on('getIntro', function (data) {
-                    var clientName = prompt('Please intro yourself to chatroom!', 'anonymous');
-                    that.socket.emit('setIntro', {name: clientName});
-                })
-                // request friendlist for first time
-                this.requestFriendList();
-            },
-
-            requestFriendList: function(){
-                var that = this;
-                that.socket.emit('requestFriendList');
-            },
-
-            receiveFriendList: function () {
-                var that = this;
-                that.socket.on('receiveFriendList', function (data) {
-                    var friends = JSON.parse(data);
-                    $('#friendlist li').not('.caption').remove();
-                    for(f in friends){
-                        $('#friendlist').append($('<li class="online">' + friends[f] + '</li>'));
-                    }
-                });
             }
+        }
 
-        }, options);
+        // extend default vaules
+        var SDNodeChatConf = $.extend(_defaults, options);
 
-        // remove trailing slash from URL if ther any using regular expression
+        // remove trailing slash from URL if there any using regular expression
         SDNodeChatConf.server = SDNodeChatConf.server.replace(/\/$/, '');
 
         // create socket object
         SDNodeChatConf.socket = io(SDNodeChatConf.server + ':' + SDNodeChatConf.port);
 
         //if connected client is new ask for intro
-        SDNodeChatConf.intro();
+        SDNodeChatConf._intro();
 
-        // Ask for friedlist every 3 min
+        // Ask for online user-list every 3 min
         setInterval(function(){
-            SDNodeChatConf.requestFriendList();
+            SDNodeChatConf.requestOnlineUsers();
         },10000);
 
-
-        // Listen for friend list and show them in list
-        SDNodeChatConf.receiveFriendList();
+        // Listen for online user list and show them in list
+        SDNodeChatConf.receiveOnlineUsers();
 
         // listen for new messages over socket
-        SDNodeChatConf.listenForMessage();
+        SDNodeChatConf.listenForMessages();
 
         // Bind click event to sendButton
         SDNodeChatConf.sendButton.click(function () {
@@ -138,7 +258,8 @@
 
         return SDNodeChatConf;
     };
-}(jQuery));
+    
+}(window, document, jQuery));
 
 
 
