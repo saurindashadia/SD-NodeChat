1f23f31ef71013c9d6a345cc253c0266f551345e
Saurin Dashadia(SD) <dev@saur.in>
2014-11-15 17:44:06 +0530

    Server side debug controlling added,

--- SD-NodeChat.js
+++ SD-NodeChat.js
@@ -10,6 +10,29 @@ var http = require("http");
 var fs = require("fs");
 var config = JSON.parse(fs.readFileSync("config.json", "UTF8"));
 
+var logMessage = function(msg, msgType, isDebug){
+    switch(msgType){
+        case 'Error':
+        case 'error':
+            msgType = 'Error   :    ';
+            break;
+
+        case 'Note':
+        case 'note':
+            msgType = 'Note    :    ';
+            break;
+
+        case 'Info':
+        case 'info':
+        default:
+            msgType = 'Info    :    ';
+            break;
+    }
+
+    if(typeof isDebug === 'undefined') isDebug = config.debug;
+    if(isDebug) console.log(msgType + msg);
+}
+
 var server = http.createServer(function (req, res) {
     switch (req.url) {
         case '/css/style.css':
@@ -17,7 +40,7 @@ var server = http.createServer(function (req, res) {
             fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
                 res.end(data)
             });
-            console.log('Info    :    CSS file served.');
+            logMessage('CSS file served.');
             break;
 
         case '/js/script.js':
@@ -25,7 +48,7 @@ var server = http.createServer(function (req, res) {
             fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
                 res.end(data)
             });
-            console.log('Info    :    JavaScript file served.');
+            logMessage('JavaScript file served.');
             break;
 
         case '/':
@@ -34,22 +57,22 @@ var server = http.createServer(function (req, res) {
             fs.readFile("./templates/chat.html", {"encoding": "utf8"}, function (error, data) {
                 res.end(data)
             });
-            console.log('Info    :    Main template served.');
+            logMessage('Main template served.');
             break;
     }
 });
 
 if(config.server && config.port){
     server.listen(config.port, config.server, function () {
-        console.log('Server listning at ' + config.server + ':' + config.port);
+        logMessage('Server listning at ' + config.server + ':' + config.port);
     });
 }else if(!config.server && config.port){
         server.listen(config.port, function () {
-            console.log('Server listning at port' + config.port);
+            logMessage('Server listning at port' + config.port);
         });
 }else{
-    console.log('Error   :    SD-NodeChat application needs minimum a port number to create a server.');
-    console.log('Note    :    Please refer the documentation here https://github.com/devsaurin/SD-NodeChat');
+    logMessage('SD-NodeChat application needs minimum a port number to create a server.', 'Error');
+    logMessage('Please refer the documentation here https://github.com/devsaurin/SD-NodeChat', 'Note');
 }
 
 var clients = [];
@@ -63,19 +86,19 @@ io.on('connection', function (socket) {
     clientID = socket.id;
     clientIP = socket.request.connection.remoteAddress;
 
-    console.log('Info    :    New client connected from server: ' + clientIP);
+    logMessage('New client connected from server: ' + clientIP);
 
     // check if connected client is known to system
     if (currentClients.indexOf(clientIP) === -1) {
         socket.emit('getIntro');
 
-        console.log('Info    :    Ask for client intro.');
+        logMessage('Ask for client intro.');
     }
 
     // set intro sent by client
     socket.on('setIntro', function (data) {
 
-        console.log('Info    :    Received client intro.');
+        logMessage('Received client intro.');
 
         clients[clientID] = {
             id: clientID,
@@ -88,7 +111,7 @@ io.on('connection', function (socket) {
 
     socket.on('requestOnlineUsers', function () {
 
-        console.log('Info    :    Received request for online users.');
+        logMessage('Received request for online users.');
 
         var friendlist = [];
         for (x in clients) {
@@ -96,18 +119,18 @@ io.on('connection', function (socket) {
         }
         this.emit('receiveOnlineUsers', JSON.stringify(friendlist));
 
-        console.log('Info    :    Online user list sent.');
+        logMessage('Online user list sent.');
     });
 
     // send received message to all clients.
     socket.on('sendMessage', function (data) {
 
-        console.log('Info    :    New message received.');
+        logMessage('New message received.');
 
         data.message = '<p class="name">' + clients[data.sender]['name'] + '</p>&nbsp;&nbsp;&nbsp;' + data.message;
         io.emit('receiveMessage', data);
 
-        console.log('Info    :    Message broadcast done.');
+        logMessage('Message broadcast done.');
     });
 });
 
