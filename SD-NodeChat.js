2a428ed0c3422fedf997104fa53f5c5c4cb42f87
Saurin Dashadia(SD) <dev@saur.in>
2014-11-21 18:15:11 +0530

    SDNodeChat object created for one point interaction.

--- SD-NodeChat.js
+++ SD-NodeChat.js
@@ -6,71 +6,102 @@
  * @license      http://opensource.org/licenses/MIT, see LICENSE
  */
 
-var http = require("http");
-var fs = require("fs");
-var config = JSON.parse(fs.readFileSync("config.json", "UTF8"));
+var SDNodeChat = {
+    _http   : require("http"),
+    _fs     : require("fs"),
+    _server : {},
+    config  : {},
 
-var logMessage = function(msg, msgType, isDebug){
-    //check if local debug flag is set, else use config value.
-    if(typeof isDebug === 'undefined') isDebug = config.debug;
+    /**
+     *
+     * @param msg
+     * @param msgType
+     * @param isDebug
+     * @returns void
+     * @usage This function is used to log application activity.
+     */
+    logMessage: function(msg, msgType, isDebug){
 
-    //log only if debug is true.
-    if(isDebug){
-        // get date object
-        var d = new Date();
-        // get full date in format 'YYYY-MM-DD hh:mm:ss'
-        var t = d.getFullYear() + '-' + ("0" + d.getMonth()).slice(-2) + '-' + ("0" + d.getDate()).slice(-2) + ' ' + ("0" + d.getHours()).slice(-2) + ':' + ("0" + d.getMinutes()).slice(-2) + ':' + ("0" + d.getSeconds()).slice(-2);
+        //check if local debug flag is set, else use config value.
+        if(typeof isDebug === 'undefined') isDebug = this.config.debug;
 
-        // capitalize first character
-        msgType = (msgType) ? (msgType.charAt(0).toUpperCase() + msgType.slice(1).toLowerCase()) : 'Error';
-        // get 8 charcter log string
-        msgType = (msgType + '        ').slice(8) + ':    ';
+        //log only if debug is true.
+        if(isDebug){
+            // get date object
+            var d = new Date();
+            // get full date in format 'YYYY-MM-DD hh:mm:ss'
+            var t = d.getFullYear() + '-' + ("0" + d.getMonth()).slice(-2) + '-' + ("0" + d.getDate()).slice(-2) + ' ' + ("0" + d.getHours()).slice(-2) + ':' + ("0" + d.getMinutes()).slice(-2) + ':' + ("0" + d.getSeconds()).slice(-2);
 
-        // log message
-        console.log('(' + t + ') ' + msgType + msg);
+            // capitalize first character
+            msgType = (msgType) ? (msgType.charAt(0).toUpperCase() + msgType.slice(1).toLowerCase()) : 'Error';
+            // get 8 charcter log string
+            msgType = (msgType + '        ').slice(8) + ':    ';
+
+            // log message
+            console.log('(' + t + ') ' + msgType + msg);
+        }
+    },
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @returns void
+     * @usage callback function for http.createServer
+     */
+    createServer_cb: function(req, res){
+        console.log(req.url);
+        switch (req.url) {
+            case '/css/style.css':
+                res.writeHead('200', {"content-Type": "text/css"});
+                SDNodeChat._fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
+                    res.end(data)
+                });
+                SDNodeChat.logMessage('CSS file served.');
+                break;
+
+            case '/js/script.js':
+                res.writeHead('200', {"content-Type": "application/javascript"});
+                SDNodeChat._fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
+                    res.end(data)
+                });
+                SDNodeChat.logMessage('JavaScript file served.');
+                break;
+
+            case '/favicon.ico':
+                break;
+
+            case '/':
+            default:
+                res.writeHead('200', {"content-Type": "text/html"});
+                SDNodeChat._fs.readFile("./templates/chat.html", {"encoding": "utf8"}, function (error, data) {
+                    res.end(data)
+                });
+                SDNodeChat.logMessage('Main template served.');
+                break;
+        }
     }
-}
+};
 
-var server = http.createServer(function (req, res) {
-    switch (req.url) {
-        case '/css/style.css':
-            res.writeHead('200', {"content-Type": "text/css"});
-            fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
-                res.end(data)
-            });
-            logMessage('CSS file served.');
-            break;
+// fetch configuration from config.json file.
+SDNodeChat.config = JSON.parse( SDNodeChat._fs.readFileSync("config.json", "UTF8") );
 
-        case '/js/script.js':
-            res.writeHead('200', {"content-Type": "application/javascript"});
-            fs.readFile("./assets" + req.url, {"encoding": "utf8"}, function (error, data) {
-                res.end(data)
-            });
-            logMessage('JavaScript file served.');
-            break;
-
-        case '/':
-        default:
-            res.writeHead('200', {"content-Type": "text/html"});
-            fs.readFile("./templates/chat.html", {"encoding": "utf8"}, function (error, data) {
-                res.end(data)
-            });
-            logMessage('Main template served.');
-            break;
-    }
+// create server.
+SDNodeChat._server = SDNodeChat._http.createServer(function(req, res){
+    SDNodeChat.createServer_cb(req,res)
 });
 
-if(config.server && config.port){
-    server.listen(config.port, config.server, function () {
-        logMessage('Server listning at ' + config.server + ':' + config.port);
+if(SDNodeChat.config.server && SDNodeChat.config.port){
+    SDNodeChat._server.listen(SDNodeChat.config.port, SDNodeChat.config.server, function () {
+        SDNodeChat.logMessage('Server listning at ' + SDNodeChat.config.server + ':' + SDNodeChat.config.port);
     });
-}else if(!config.server && config.port){
-        server.listen(config.port, function () {
-            logMessage('Server listning at port' + config.port);
-        });
+}else if(!SDNodeChat.config.server && SDNodeChat.config.port){
+    SDNodeChat._server.listen(SDNodeChat.config.port, function () {
+        SDNodeChat.logMessage('Server listning at port' + SDNodeChat.config.port);
+    });
 }else{
-    logMessage('SD-NodeChat application needs minimum a port number to create a server.', 'Error');
-    logMessage('Please refer the documentation here https://github.com/devsaurin/SD-NodeChat', 'Note');
+    SDNodeChat.logMessage('SD-NodeChat application needs minimum a port number to create a server.', 'Error');
+    SDNodeChat.logMessage('Please refer the documentation here https://github.com/devsaurin/SD-NodeChat', 'Note');
 }
 
 var clients = [];
@@ -78,7 +109,7 @@ var currentClients = [];
 var clientID = null;
 var clientIP = null;
 
-var io = require("socket.io")(server);
+var io = require("socket.io")(SDNodeChat._server);
 
 io.on('connection', function (socket) {
     clientID = socket.id;
@@ -87,11 +118,11 @@ io.on('connection', function (socket) {
     // check if connected client is known to system
     if (currentClients.indexOf(clientIP) === -1) {
 
-        logMessage('New client connected from server: ' + clientIP);
+        SDNodeChat.logMessage('New client connected from server: ' + clientIP);
 
         socket.emit('getIntro');
 
-        logMessage('Ask for client intro.');
+        SDNodeChat.logMessage('Ask for client intro.');
     }
 
     // set intro sent by client
@@ -103,14 +134,14 @@ io.on('connection', function (socket) {
             ip: clientIP
         };
 
-        logMessage('Received client intro.');
+        SDNodeChat.logMessage('Received client intro.');
 
         currentClients.push(clientIP);
     });
 
     socket.on('requestOnlineUsers', function () {
 
-        logMessage('Received request for online users.');
+        SDNodeChat.logMessage('Received request for online users.');
 
         var friendlist = [];
         for (x in clients) {
@@ -118,23 +149,22 @@ io.on('connection', function (socket) {
         }
         this.emit('receiveOnlineUsers', JSON.stringify(friendlist));
 
-        logMessage('Online user list sent.');
+        SDNodeChat.logMessage('Online user list sent.');
     });
 
     // send received message to all clients.
     socket.on('sendMessage', function (data) {
 
-        logMessage('New message received.');
+        SDNodeChat.logMessage('New message received.');
 
         data.message = '<p class="name">' + clients[data.sender]['name'] + '</p>&nbsp;&nbsp;&nbsp;' + data.message;
         io.emit('receiveMessage', data);
 
-        logMessage('Message broadcast done.');
+        SDNodeChat.logMessage('Message broadcast done.');
     });
 
     socket.on('disconnect', function(){
-        logMessage('Client left. ' + clients[clientID]);
+        SDNodeChat.logMessage('Client left. ' + clients[clientID]);
         delete clients[clientID];
     })
 });
-
